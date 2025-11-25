a343b527a60bd8a10d3de2df68c4444a00638688
Neurocollective <31573287+neurocollective@users.noreply.github.com>
2025-11-25 20:07:47 +0000

    got basic queries running (#1)

--- Makefile
+++ Makefile
@@ -3,3 +3,17 @@ server/start:
 	python3 -m flask start;
 server/venv:
 	python3 -m venv ./server
+db/create:
+	docker run -p 5432:5432 --name local-postgres -e POSTGRES_PASSWORD=postgres -d postgres
+db/psql:
+	docker exec -it local-postgres psql "postgresql://postgres:postgres@localhost:5432/postgres"
+db/seed:
+	psql -f db/create.sql "postgresql://postgres:postgres@localhost:5432/postgres"
+	psql -f db/seed.sql "postgresql://postgres:postgres@localhost:5432/postgres"
+db/kill:
+	docker stop local-postgres && docker rm local-postgres
+db/recycle:
+	make kill
+	make db/create
+	sleep 2
+	make db/seed
--- db/create.sql
+++ db/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE gresy_user (
-	id int PRIMARY KEY serial,
+	id serial PRIMARY KEY,
 	email text NOT NULL,
 	password_hash text NOT NULL,
 	created_date timestamp NOT NULL default now(),
@@ -7,24 +7,43 @@ CREATE TABLE gresy_user (
 );
 
 CREATE TABLE city (
-	id int PRRIMARY KEY,
+	id serial PRIMARY KEY,
 	name text NOT NULL,
 	created_date timestamp NOT NULL default now(),
 	modified_date timestamp NOT NULL default now()
-)
+);
+
+CREATE TABLE borough (
+	id serial PRIMARY KEY,
+	city_id int REFERENCES city(id) NOT NULL,
+	name text NOT NULL,
+	created_date timestamp NOT NULL default now(),
+	modified_date timestamp NOT NULL default now()
+);
+
+CREATE TABLE neighborhood (
+	id serial PRIMARY KEY,
+	city_id int REFERENCES city(id) NOT NULL,
+	borough_id int REFERENCES borough(id),
+	name text NOT NULL,
+	created_date timestamp NOT NULL default now(),
+	modified_date timestamp NOT NULL default now()
+);
 
 CREATE TABLE restaurant (
-	id int PRIMARY KEY serial,
+	id serial PRIMARY KEY,
 	name text NOT NULL,
 	address text NOT NULL,
 	city_id int REFERENCES city(id) NOT NULL,
-	secondary_city_id REFERENCES city(id),
-	created_date timestamp NOT NULLdefault now(),
+	borough_id int REFERENCES borough(id),
+	neighborhood_id int REFERENCES neighborhood(id),
+	secondary_city_id int REFERENCES city(id),
+	created_date timestamp NOT NULL default now(),
 	modified_date timestamp NOT NULL default now()
 );
 
 CREATE TABLE seating_inventory (
-	id int PRIMARY KEY serial,
+	id serial PRIMARY KEY,
 	restaurant_id int NOT NULL REFERENCES restaurant(id),
 	seat_count int NOT NULL,
 	quantity int NOT NULL,
@@ -35,8 +54,30 @@ CREATE TABLE seating_inventory (
 );
 
 CREATE TABLE inventory_reservation (
-	id int PRIMARY KEY serial,
+	id serial PRIMARY KEY,
 	seating_inventory_id int NOT NULL REFERENCES seating_inventory(id),
 	created_date timestamp NOT NULL default now(),
 	modified_date timestamp NOT NULL default now()
 );
+
+-- end basic tables
+
+-- end augment tables
+
+CREATE TABLE access_level (
+	id serial PRIMARY KEY,
+	name text NOT NULL,
+	created_date timestamp NOT NULL default now(),
+	modified_date timestamp NOT NULL default now()
+);
+
+CREATE TABLE admin_user (
+	id serial PRIMARY KEY,
+	email text NOT NULL,
+	access_level int REFERENCES access_level(id),
+	password_hash text NOT NULL,
+	created_date timestamp NOT NULL default now(),
+	modified_date timestamp NOT NULL default now()
+);
+
+-- end admin tables
