06be2a85d2505d750f8d8b673046ceb44d83a149
David Ashe <david@neurocollective.tech>
2025-11-25 16:28:30 -0500

    more tables, changing reservation shape

--- db/create.sql
+++ db/create.sql
@@ -1,7 +1,10 @@
 CREATE TABLE gresy_user (
 	id serial PRIMARY KEY,
-	email text NOT NULL,
-	password_hash text NOT NULL,
+	phone text NOT NULL,
+	name text,
+	username text,
+	email text,
+	password_hash text,
 	created_date timestamp NOT NULL default now(),
 	modified_date timestamp NOT NULL default now()
 );
@@ -42,11 +45,26 @@ CREATE TABLE restaurant (
 	modified_date timestamp NOT NULL default now()
 );
 
+CREATE TABLE restaurant_tags (
+	id serial PRIMARY KEY,
+	name text NOT NULL,
+	created_date timestamp NOT NULL default now(),
+	modified_date timestamp NOT NULL default now()
+);
+
+CREATE TABLE restaurant_tag_assignment (
+	id serial PRIMARY KEY,
+	restaurant_id int REFERENCES restaurant(id) NOT NULL,
+	tag_id int REFERENCES restaurant_tags(id) NOT NULL,
+	created_date timestamp NOT NULL default now(),
+	modified_date timestamp NOT NULL default now()
+);
+
 CREATE TABLE seating_inventory (
 	id serial PRIMARY KEY,
 	restaurant_id int NOT NULL REFERENCES restaurant(id),
 	seat_count int NOT NULL,
-	quantity int NOT NULL,
+	day date NOT NULL,
 	begin_time timestamp NOT NULL,
 	end_time timestamp NOT NULL,
 	created_date timestamp NOT NULL default now(),
@@ -55,7 +73,8 @@ CREATE TABLE seating_inventory (
 
 CREATE TABLE inventory_reservation (
 	id serial PRIMARY KEY,
-	seating_inventory_id int NOT NULL REFERENCES seating_inventory(id),
+	user_id int REFERENCES gresy_user(int) NOT NULL,
+	seating_inventory_id int REFERENCES seating_inventory(id) NOT NULL,
 	created_date timestamp NOT NULL default now(),
 	modified_date timestamp NOT NULL default now()
 );
