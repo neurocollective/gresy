8bdadfb24efb86161315a5783b0eab048244e7fe
David Ashe <david@neurocollective.tech>
2025-11-25 15:33:37 -0500

    removing & ignoring __pycache__ folders

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import sqlalchemy
 from sqlalchemy import create_engine, text
+from queries import *
 
 app = Flask(__name__)
 
@@ -10,7 +11,23 @@ connection = engine.connect()
 
 @app.route("/users")
 def get_users():
-    rows = connection.execute(text("SELECT * FROM gresy_user;"))
+    users_query = get_users_query()
+    query_text = users_query[0]
+    rows = connection.execute(text(query_text))
+
+    all_dict_rows = []
+
+    for row in rows:
+        all_dict_rows.append({ "email": row[1] })
+
+    return all_dict_rows
+
+@app.route("/restaurants/<city>")
+def get_restaurants(city):
+    users_query = get_restaurants_query(city)
+    query_text = users_query[0]
+    query_params = users_query[1]
+    rows = connection.execute(text(query_text), query_params)
 
     all_dict_rows = []
 
@@ -21,7 +38,7 @@ def get_users():
 
 @app.route("/message")
 def message():
-    return { "message": "yo"}
+    return { "message": "duder" }
 
 @app.route("/")
 def hello(name=None):
